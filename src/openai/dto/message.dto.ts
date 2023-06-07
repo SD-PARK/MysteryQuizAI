df4893c1ec444f0c0c50895b113e522c95788a53
SD-PARK <tkdeh129@naver.com>
2023-06-07 13:25:29 +0900

    refactor: Body 값의 유효성 검사를 위한 messagesDto 생성

--- src/openai/dto/message.dto.ts
+++ src/openai/dto/message.dto.ts
@@ -1,4 +1,5 @@
-import { IsIn, IsString } from "class-validator";
+import { Type } from "class-transformer";
+import { IsIn, IsString, ValidateNested } from "class-validator";
 
 /**
  * OpenAI API에 전송되는 요청에 사용되는 메시지 DTO입니다.
@@ -9,4 +10,10 @@ export class MessageDto {
 
     @IsString()
     readonly content: string;
+}
+
+export class MessagesDto {
+    @Type(() => MessageDto)
+    @ValidateNested({ each: true })
+    readonly messages: MessageDto[];
 }
