b77935ebb6894054ac6741d51df943f21cef39fe
SD-PARK <tkdeh129@naver.com>
2023-06-07 11:55:03 +0900

    feat: ValidationPipe 설정

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,21 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   dotenv.config();
-  
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // Static
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  
+  // Pipes
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
   await app.listen(3000);
 }
 bootstrap();
