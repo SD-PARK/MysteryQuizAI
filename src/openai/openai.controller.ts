c6879a3d17753b637668398af2338b04f9620c81
SD-PARK <tkdeh129@naver.com>
2023-06-07 11:55:36 +0900

    feat: 주석 및 Post('inquiry') 추가

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
+import { MessageDto } from './dto/message.dto';
 
+/**
+ * OpenAI API와 관련된 기능을 제공합니다.
+ * 새로운 게임을 생성하고, 사용자와 AI 간의 대화를 관리합니다.
+ */
 @Controller('openai')
 export class OpenaiController {
     constructor(private readonly openaiService:OpenaiService) {}
 
     @Get()
-    async generateNewQuiz(): Promise<string> {
-        return await this.openaiService.generateNewQuiz();
+    async generateNewGame(): Promise<string> {
+        return await this.openaiService.generateNewGame();
+    }
+
+    @Post('inquiry')
+    async inquiry(@Body() messagesData: MessageDto[]): Promise<string> {
+        console.log(messagesData);
+        return 'good';
     }
 }
