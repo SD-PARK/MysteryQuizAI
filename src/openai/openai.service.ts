e2ab3ceb61829cff3f956344a569ab116a169c99
SD-PARK <tkdeh129@naver.com>
2023-06-07 13:32:46 +0900

    refactor: AI 답변의 퀄리티 상승을 위해 조건문 추가

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -7,7 +7,7 @@ export class OpenaiService {
     private config: Configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
     private openai: OpenAIApi = new OpenAIApi(this.config);
     private readonly scenario: MessageDto[] = [{role: 'system', content: `당신은 게임 마스터입니다.`},
-                                             {role: 'user', content: `범죄 추리 게임을 만들어보세요. 제가 조사할 무작위 시나리오를 생성하세요. 여러 명의 용의자를 만들고, 이 채팅을 통해 질문하겠습니다. 제가 'TALKTO' 명령어를 사용하면, 해당 캐릭터와 대화할 수 있습니다. 제가 'TALKTO' 명령어를 통해 한 가지를 질문하면, 캐릭터가 한 가지를 대답하고, 다음 질문을 대기합니다. 우리는 번갈아가며 하나씩 질문하고 답변할 것입니다. 제가 'INV' 명령어를 사용하면, 범죄 현장을 조사할 수 있습니다. 범죄 현장을 신체, 머리, 눈 또는 책상, 서랍 상자와 같은 여러 개의 구성 요소로 분리합니다. 각 구성 요소를 살펴보면서, 조사와 관련된 정보를 추가해주세요. 범죄에 대한 단서들을 발견하면, 증거 목록을 저장해 유지해주세요. 제가 'EVIDENCE' 명령어를 사용하면, 조사를 통해 알게된 모든 단서를 나열해주세요.`}];
+                                             {role: 'user', content: `범죄 추리 게임을 만들어보세요. 제가 조사할 무작위 시나리오를 생성하세요. 여러 명의 용의자를 만들고, 이 채팅을 통해 질문하겠습니다. 제가 'TALKTO' 명령어를 사용하면, 해당 캐릭터와 대화할 수 있습니다. 제가 'TALKTO' 명령어를 통해 한 가지를 질문하면, 캐릭터가 한 가지를 대답하고, 다음 질문을 대기합니다. 우리는 번갈아가며 한 차례에 하나씩만 질문하고 답변할 것입니다. 명심하세요. 한 차례에 하나만 질문하고, 하나만 답변합니다. 제가 'INV' 명령어를 사용하면, 범죄 현장을 조사할 수 있습니다. 범죄 현장을 신체, 머리, 눈 또는 책상, 서랍 상자와 같은 여러 개의 구성 요소로 분리합니다. 각 구성 요소를 살펴보면서, 조사와 관련된 정보를 추가해주세요. 범죄에 대한 단서들을 발견하면, 증거 목록을 저장해 유지해주세요. 제가 'EVIDENCE' 명령어를 사용하면, 조사를 통해 알게된 모든 단서를 나열해주세요.`}];
     private logger: Logger = new Logger(OpenaiService.name);
 
     /**
@@ -24,7 +24,6 @@ export class OpenaiService {
      */
     async inquiry(messagesData: MessagesDto): Promise<string> {
         const messages: MessageDto[] = [...this.scenario, ...messagesData.messages];
-        console.log(messages.length);
 
         return await this.createChatCompletion(messages);
     }
