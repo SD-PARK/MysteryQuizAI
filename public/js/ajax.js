8830feeaf91090f90bd24fc6949afd6ddbf67d50
SD-PARK <tkdeh129@naver.com>
2023-06-07 11:54:12 +0900

    feat: ajax 통신 관련 js

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
     const inputText = $('#input-text');
     const chatMessages = $('#chat-messages');
+    const messages = [];
+
+    function pushMessages(role, content) {
+        messages.push({
+            role: role,
+            content: content
+        });
+    }
 
     // 메시지 전송 이벤트 진입 관련
-    $('#send-button').click(function() { askQuestion() });
+    $('#send-button').click(function() { inquiry() });
     $('#input-text').keydown(function(event) {
         if (event.keyCode === 13 && !event.shiftKey && sendPossible) {
-            askQuestion();    
+            inquiry();    
             event.preventDefault();
         }
     });
 
-    function askQuestion() {
-        const data = { message: inputText.val() };
+    /**
+     * 새로운 게임을 요청합니다.
+     */
+    function newGame() {
         sendPossible = false;
-        inputText.val('');
         buttonDisabled();
 
         $.ajax({
@@ -25,6 +34,37 @@ $(document).ready(function() {
                 chatMessages.html(response);
                 sendPossible = true;
                 checkSendPossible();
+                pushMessages('assistant', response);
+            },
+            error: function(xhr, status, error) {
+                chatMessages.html('문제를 불러올 수 없어요.\n새로고침 해주세요.');
+                sendPossible = true;
+                checkSendPossible();
+                console.error('Error:', error);
+            }
+        });
+    }
+
+    /**
+     * 현재 게임에 대한 조사를 진행합니다.
+     */
+    function inquiry() {
+        const data = { message: inputText.val() };
+        sendPossible = false;
+        inputText.val('');
+        buttonDisabled();
+
+        $.ajax({
+            type: 'POST',
+            url: '/openai',
+            data: [...messages, {role: 'user', content: data}],
+            contentType: 'application/json',
+            success: function(response) {
+                chatMessages.html(response);
+                sendPossible = true;
+                checkSendPossible();
+                pushMessages('user', data);
+                pushMessages('assistant', response);
             },
             error: function(xhr, status, error) {
                 console.error('Error:', error);
@@ -34,4 +74,6 @@ $(document).ready(function() {
             }
         });
     }
+
+    newGame();
 });
