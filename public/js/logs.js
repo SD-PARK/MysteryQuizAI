13d746f017f2093890cf87a5188679a4396b8919
SD-PARK <tkdeh129@naver.com>
2023-06-08 12:11:34 +0900

    style: 시간 초과 시 메시지 출력문 번경

--- public/js/logs.js
+++ public/js/logs.js
@@ -22,7 +22,7 @@ function replaceLoadingLog(status, content = '') {
         loadingDiv.html(content);
     } else if (status === 400) {
         updateProgressBar(4097);
-        loadingDiv.html(`시간이 초과되었습니다.<br>사건은 종결되었으며, 충분한 단서를 수집하지 못해<br>사건의 결론은 영영 알 수 없는 미궁 속으로 빠지게 되었습니다.<br>새로운 게임을 시작하려면 'NEWGAME' 명령어를 입력하세요!`);
+        loadingDiv.html(`시간이 초과되었습니다.<br>사건은 종결되었으며, 충분한 단서를 수집하지 못해 사건의 결론은 영영 알 수 없는 미궁 속으로 빠지게 되었습니다.<br>새로운 게임을 시작하려면 'NEWGAME' 명령어를 입력하세요!`);
     } else {
         loadingDiv.html(`문제를 불러오지 못했어요.<br>다시 질문해주세요.`);
         setTimeout(() => { loadingDiv.remove(); }, 3000);
