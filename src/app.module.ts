c4a7b724076d89c8febaf944e38e29b199c40f19
SD-PARK <tkdeh129@naver.com>
2023-06-08 15:36:32 +0900

    refactor: OPENAI_API_KEY 로드 방식 변경 dotenv -> @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { OpenaiModule } from './openai/openai.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [OpenaiModule],
+  imports: [OpenaiModule,
+            ConfigModule.forRoot({
+              cache: true,
+              isGlobal: true,
+            }),],
   controllers: [AppController],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // Static
   app.useStaticAssets(join(__dirname, '..', 'public'));
