227d22a09f520459808dbf03d83b4377739a7eed
SD-PARK <tkdeh129@naver.com>
2023-06-08 21:16:46 +0900

    comment: 주석 추가

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // Static
+  // 정적 파일 경로 설정
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  // 뷰 엔진 설정
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  // Pipes
+  // 전역 파이프 설정
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
