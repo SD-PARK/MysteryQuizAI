d91b144da89192ffaa5abf644316f19635d88bc3
SD-PARK <tkdeh129@naver.com>
2023-06-08 21:15:44 +0900

    refactor: TextWithTokenCountDto의 속성에 readonly 추가

--- src/openai/dto/TextWithTokenCount.dto.ts
+++ src/openai/dto/TextWithTokenCount.dto.ts
@@ -2,6 +2,6 @@
  * OpenAI API를 통해 생성한 텍스트와 현재 토큰 사용량이 포함된 DTO 입니다.
  */
 export class TextWithTokenCountDto {
-    content: string;
-    token_count: number;
+    readonly content: string;
+    readonly token_count: number;
 }
