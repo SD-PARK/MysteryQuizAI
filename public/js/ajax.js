e968ea3ccf666e43c476e92f3e2f88cc497bd8fe
SD-PARK <tkdeh129@naver.com>
2023-06-08 21:16:20 +0900

    refactor: 초기 게임 설명 시 가이드 변경

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -13,7 +13,8 @@ function pushMessages(role, content) {
     });
 }
 
-const manual = `당신은 정해진 시간 내에 사건에 대한 단서를 수집하고, 결론을 도출해내어야 합니다.<br>주어진 명령어와 기지를 발휘해 최소한의 질문으로 게임을 클리어해보세요!<br><br>게임 명령어:<br>1. TALKTO [용의자] - 용의자와 대화합니다.<br>2. INV - 범죄 현장을 조사합니다.<br>3. EVIDENCE - 획득한 증거 목록을 나열합니다.<br>4. NEWGAME - 새로운 게임을 시작합니다.<br><br>`;
+const guide = `당신은 정해진 시간 내에 사건에 대한 단서를 수집하고, 결론을 도출해내어야 합니다.<br>주어진 명령어와 기지를 발휘해 최소한의 질문으로 게임을 클리어해보세요!<br><br>`
+const commandGuide = `<br><br>게임 명령어:<br>1. TALKTO [용의자] - 용의자와 대화합니다.<br>2. INV - 범죄 현장을 조사합니다.<br>3. EVIDENCE - 획득한 증거 목록을 나열합니다.<br>4. NEWGAME - 새로운 게임을 시작합니다.`;
 /** 새로운 게임을 요청합니다. */
 function newGame() {
     sendPossible = false;
@@ -26,7 +27,7 @@ function newGame() {
         url: '/openai',
         contentType: 'application/json',
         success: function(response) {
-            chatMessages.html(manual + response.content);
+            chatMessages.html(guide + response.content + commandGuide);
             sendPossible = true;
             checkSendPossible();
             pushMessages('assistant', response.content);
