e56026127ee0615f5c879a69973d83c69bad0d0b
SD-PARK <tkdeh129@naver.com>
2023-06-09 14:55:09 +0900

    feat: 제한 시간 안내 Modal 추가

--- public/js/action.js
+++ public/js/action.js
@@ -1,6 +1,7 @@
 const inputText = document.getElementById('input-text');
 const sendButton = document.getElementById('send-button');
 const progress = document.getElementById('progress-step');
+const chatContainer = $('#chat-container');
 /** 
  * 메시지 전송 가능 여부를 나타내는 변수
  * 이 변수는 메시지를 전송한 후 답변이 도착하기 전까지 전송을 금지하기 위해 사용됩니다.
@@ -64,6 +65,13 @@ function initProgressBar(tokens) {
  * @param {number} tokens - 현재 토큰의 개수입니다.
  */
 function updateProgressBar(tokens) {
+    const half = progress.max * 0.5;
+    const fewLeft = progress.max * 0.85;
+    if (half > progress.value && half <= (tokens - initialTokenValue)) {
+        modalFade(true);
+    } else if (fewLeft > progress.value && fewLeft <= (tokens - initialTokenValue)) {
+        modalFade(false);
+    }
     progress.value = tokens - initialTokenValue;
 }
 
@@ -73,4 +81,25 @@ function updateProgressBar(tokens) {
 function emptyTextarea() {
     $('#input-text').val('');
     buttonDisabled();
+}
+
+
+/**
+ * 제한 시간이 일정 수치를 넘었을 때, 팝업 창을 출력합니다.
+ * @param {boolean} halftime - 제한 시간이 절반 지났다면 True, 아니라면 False.
+ */
+function modalFade(halftime) {
+    chatContainer.append(`<div id="modal"></div>`);
+    const modal = $('#modal');
+
+    if (halftime) { modal.html(`제한 시간이 절반 남았습니다!`); }
+    else { modal.html(`제한 시간이 얼마 남지 않았습니다!<br>현재까지 모인 단서를 통해 범인을 지목하고, 그 이유를 설명하세요!`); }
+
+    modal.animate({ "opacity": '0.9' }, 300, 'swing');
+    setTimeout(() => {
+        modal.animate({ "opacity": '0' }, 300, 'swing');
+    }, 4500);
+    setTimeout(() => {
+        modal.remove();
+    }, 5000);
 }
