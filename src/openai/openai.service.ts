37d1d622a7946f38f152fa413f8e10645fd429f0
SD-PARK <tkdeh129@naver.com>
2023-06-09 12:18:07 +0900

    refactor: 문제에 범인을 명시하지 않도록 조건 추가

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -10,7 +10,7 @@ export class OpenaiService {
     private config: Configuration = new Configuration({ apiKey: this.configService.get<string>('OPENAI_API_KEY') });
     private openai: OpenAIApi = new OpenAIApi(this.config);
     private readonly scenario: MessageDto[] = [{role: 'system', content: `당신은 추리 게임의 관리자입니다. user가 'TALKTO' 명령어를 사용하면, 용의자는 user의 질문을 대기합니다. 질문은 한 가지만 받을 수 있고, 용의자도 한 가지만 대답한 뒤, 다음 질문을 대기합니다. user가 'INV' 명령어를 사용하면, 범죄 현장을 조사할 수 있습니다. 당신은 범죄 현장을 피해자의 신체와 범죄 현장 주변의 있을 법한 구성요소로 분리합니다. user의 질문을 통해 각 구성요소를 살펴보다 범죄에 대한 단서를 발견하면, 증거 목록을 저장해 유지하세요. user가 'EVIDENCE' 명령어를 사용하면, 조사를 통해 알게된 모든 단서를 나열하세요. 사건에 대한 조사는 user만 가능하며, user가 명령어를 통해 조사하면, 정답을 추리할 수 있는 단서가 제공되어야 합니다.`},
-                                                {role: 'user', content: `저는 user입니다. 제가 탐정이 되어 사건을 조사할 것입니다. 제가 조사하게 될 무작위 미해결 범죄 사건을 만드세요. 사건의 개요를 설명하고, 5명 이하의 용의자를 만들어 나열하고, 정답에 대한 단서를 나열한 뒤, 저의 질문을 기다리세요.`}];
+                                                {role: 'user', content: `저는 user입니다. 제가 탐정이 되어 사건을 조사할 것입니다. 제가 조사하게 될, 범인이 잡히지 않은 무작위 미해결 범죄 사건을 만드세요. 사건의 개요를 설명하고, 5명 이하의 용의자를 만들어 나열하고, 정답에 대한 단서를 나열한 뒤, 저의 질문을 기다리세요.`}];
     private logger: Logger = new Logger(OpenaiService.name);
 
     /**
